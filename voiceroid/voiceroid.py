800dc80ec1f36dd67acf92542fa1119b2249a749
LeavaTail <starbow.duster@gmail.com>
2024-12-08 20:20:44 +0900

    introduce main function

--- voiceroid/voiceroid.py
+++ voiceroid/voiceroid.py
@@ -4,22 +4,26 @@ clr.AddReference('WCFClient')
 import AssistantSeika
 
 from openai import OpenAI
- 
-completion = OpenAI().chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
 
-get_response = completion.choices[0].message.content
-print(get_response)
+def main():
+    completion = OpenAI().chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": "Write a haiku about recursion in programming."
+            }
+        ]
+    )
 
-client = AssistantSeika.WCFClient()
- 
-# 発声
-pt = client.Talk(5203, get_response, None, None)
+    get_response = completion.choices[0].message.content
+    print(get_response)
+
+    client = AssistantSeika.WCFClient()
+    
+    # 発声
+    pt = client.Talk(5203, get_response, None, None)
+
+if __name__ == "__main__":
+    main()
