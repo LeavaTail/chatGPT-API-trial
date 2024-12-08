1ff0d2371bfd415fafe80c5bfce7fbd9ce1ffed7
LeavaTail <starbow.duster@gmail.com>
2024-12-08 20:11:25 +0900

    Add OpenAPI features

--- voiceroid/voiceroid.py
+++ voiceroid/voiceroid.py
@@ -2,8 +2,24 @@ import clr
  
 clr.AddReference('WCFClient')
 import AssistantSeika
+
+from openai import OpenAI
  
+completion = OpenAI().chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+get_response = completion.choices[0].message.content
+print(get_response)
+
 client = AssistantSeika.WCFClient()
  
 # 発声
-pt = client.Talk(5203, "pythonから呼べましたね", None, None)
+pt = client.Talk(5203, get_response, None, None)
